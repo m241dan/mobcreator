a35ec5598d088312411f12130eadacff6087845b
Davenge <korisd@gmail.com>
2015-09-30 18:51:19 -0400

    ok, got it runnin slick as shit

--- creator.lua
+++ creator.lua
@@ -3,17 +3,23 @@ require( "db" )
 
 shutdown = false
 
-function main()
-   -- initialize the db
+local function main()
    local input
+
+   -- initialize the db
    db.start()
    sql_handle = db.newSql( sqlcfg.DB_NAME, sqlcfg.DB_LOGIN, sqlcfg.DB_PASS, sqlcfg.DB_ADDR )
 
+   -- get the interpreter coroutine and initialize it
    interp = require( "interpreter" );
    interp();
 
+   -- a welcoming message
+   print( "\nWelcome to the FFXI MobCreator\nAt its heart, this is a simple tool to interface with and create or edit database data.\nIt could easilly be extended with additional lua types." )
+
+   -- the input grabbing loop
    while( shutdown ~= true ) do
-      input = io.read()
+      input = io.read("*line")
       if( input == "shutdown" ) then
          shutdown = true
       else
@@ -21,7 +27,9 @@ function main()
       end
    end
 
+   -- the clean up
    db.shutdown()
+   interp = nil
 end
 
 main()
--- interpreter.lua
+++ interpreter.lua
@@ -1,10 +1,18 @@
+local prompt = {
+                  [1] = "Commands: Open or New: "
+               }
+
 function interpret()
-   local input
+   local input, state = nil, 1
 
    print( "Interpreter thread booting up." )
+
    while( true ) do
       input = coroutine.yield()
-      print( "The input is a " .. input )
+      if( input ~= nil and input ~= "" ) then
+         print( "The input is a " .. input )
+      end
+      io.write( prompt[state] )
    end
 end
 
